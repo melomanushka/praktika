de2e4da0118fb5d8cc5255466fde171115622218
melomanushka <zitihinaDA@cloud.biik.ru>
2023-12-14 19:20:53 +0800

    Добавьте файлы проекта.

---
+++ praktika/Views/Login-Page.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace praktika.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Login_Page.xaml
+    /// </summary>
+    public partial class Login_Page : Page
+    {
+        public Login_Page()
+        {
+            InitializeComponent();
+        }
+
+        private void nextPageButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Registration_Page());
+        }
+    }
+}
---
+++ praktika/Views/Registration-Page.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace praktika.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration_Page.xaml
+    /// </summary>
+    public partial class Registration_Page : Page
+    {
+        public Registration_Page()
+        {
+            InitializeComponent();
+        }
+
+        bool isPressed = false;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Login_Page());
+        }
+
+        private void Theme_Change_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isPressed)
+            {
+                themeButton.Background = new ImageBrush(new BitmapImage(new Uri("D:\\визуалка проекты\\praktika\\praktika\\image\\moon.png")));
+                isPressed= true;
+            }
+
+            else if (isPressed)
+            {
+                themeButton.Background = new ImageBrush(new BitmapImage(new Uri("D:\\визуалка проекты\\praktika\\praktika\\image\\sun.png")));
+                isPressed = false;
+            }
+        }
+    }
+}
